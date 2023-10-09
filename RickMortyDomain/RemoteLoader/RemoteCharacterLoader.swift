b49b3aea592b3ac5ef5dd8cd2ce6a41629dd6056
Joao Matheus <joao.rocha@corebiz.ag>
2023-10-09 18:43:40 -0300

    Guaranteeing that test `RickMortyDomain.Load` returned error for invalid data.

--- RickMortyDomain/RemoteLoader/RemoteCharacterLoader.swift
+++ RickMortyDomain/RemoteLoader/RemoteCharacterLoader.swift
@@ -12,23 +12,37 @@ public struct Character: Decodable, Equatable {
     
 }
 
+public struct CharacterRoot: Decodable {
+    let results: [Character]
+}
+
 public final class RemoteCharacterLoader: RickMortyLoader {
     public typealias T = Result<[Character], RickMortyResultError>
 
     let networkClient: NetworkClient
     let fromUrl: URL
+    let okResponse: Int = 200
 
     init(networkClient: NetworkClient, fromUrl: URL) {
         self.networkClient = networkClient
         self.fromUrl = fromUrl
     }
 
+    private func successfullyValidation(_ data: Data, response: HTTPURLResponse) -> T {
+        guard let json = try? JSONDecoder().decode(CharacterRoot.self, from: data),
+              response.statusCode == okResponse else {
+            return .failure(.invalidData)
+        }
+        return .success(json.results)
+    }
+    
     public func load(completion: @escaping (Result<[Character], RickMortyResultError>) -> Void) {
         networkClient.request(from: fromUrl) { [weak self] result in
             guard let self else { return }
             switch result {
+            case let .success((data, response)):
+                completion(successfullyValidation(data, response: response))
             case .failure: completion(.failure(.withoutConnectivity))
-            default: break
             }
         }
     }
--- RickMortyDomainTests/CharacterDomainTests.swift
+++ RickMortyDomainTests/CharacterDomainTests.swift
@@ -10,7 +10,7 @@ import NetworkClient
 @testable import RickMortyDomain
 
 final class CharacterDomainTests: XCTestCase {
-    typealias resultType = Result<[Character], RickMortyResultError>
+    typealias ResultType = Result<[Character], RickMortyResultError>
 
     func test_initializer_remoteRestaurantLoader_and_validate_urlRequest() {
         let (sut, spy, anyUrl) = makeSUT()
@@ -31,27 +31,47 @@ final class CharacterDomainTests: XCTestCase {
 
     func test_load_returned_error_for_connectivity() {
         let (sut, spy, _) = makeSUT()
-        let result: resultType = .failure(.withoutConnectivity)
+        let result: ResultType = .failure(.withoutConnectivity)
         
-        let exp = expectation(description: "waiting")
-        var returnedResult: resultType?
-        sut.load { result in
-            returnedResult = result
-            exp.fulfill()
+        assert(sut, completion: .failure(.withoutConnectivity)) {
+            spy.completionWithError()
         }
-        
-        spy.completionWithError()
-        
-        wait(for: [exp], timeout: 1.0)
-        XCTAssertEqual(returnedResult, result)
+
     }
 
+    func test_load_and_returned_error_for_invalidData() {
+        let (sut, spy, _) = makeSUT()
+
+        assert(sut, completion: .failure(.invalidData)) {
+            spy.completionWithSuccess()
+        }
+
+    }
+    
     private func makeSUT() -> (RemoteCharacterLoader, NetworkClientSpy, URL) {
         let spy = NetworkClientSpy()
         let anyUrl = URL(string: "https://rickandmortyapi.com/")!
         let sut = RemoteCharacterLoader(networkClient: spy, fromUrl: anyUrl)
         
         return (sut, spy, anyUrl)
+    }
+    
+    private func assert(
+        _ sut: RemoteCharacterLoader,
+        completion result: ResultType,
+        when action: () -> Void
+    ) {
+        let exp = expectation(description: "waiting")
+        var returnedResult: ResultType?
+        sut.load { result in
+            returnedResult = result
+            exp.fulfill()
+        }
+        
+        action()
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(returnedResult, result)
     }
 }
 
@@ -67,6 +87,16 @@ final class NetworkClientSpy: NetworkClient {
 
     func completionWithError() {
         completionHandler?(.failure(anyError()))
+    }
+    
+    func completionWithSuccess(data: Data = Data(), statusCode: Int = 200) {
+        let response = HTTPURLResponse(
+            url: urlRequest[0],
+            statusCode: statusCode,
+            httpVersion: nil,
+            headerFields: nil
+        )!
+        completionHandler?(.success((data, response)))
     }
     
     private func anyError() -> Error {
